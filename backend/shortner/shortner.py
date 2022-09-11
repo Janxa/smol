b943ddd9cc5f3f0ef997807b1237a1fe5f2d70ae
Janxa <onizuka-54@live.fr>
2022-09-11 08:35:04 +0000

    The create url form is now working properly, generate url function now generates urls based on the user's alias choice

--- backend/shortner/shortner.py
+++ backend/shortner/shortner.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 from flask import Flask, request, Blueprint
 import secrets
 from backend.extensions import url_col
@@ -6,16 +7,24 @@ shortner = Blueprint('shortner',__name__, url_prefix='/shortner')
 @shortner.route('',methods=['POST'])
 def get_urls():
     x = request.get_json()
-    long,alias = x['long'],x['alias']
-    generate_url( long,alias)
-    return x 
+    long,alias,allowMod = x['url'],x['alias'],x['allowMod']
+    short  = generate_url( long,alias,allowMod)
+    return short
 
-def generate_url(long,alias=''):
+def generate_url(long,alias='',allowMod=False):
     if alias == '':
         short = "http://smol/" + secrets.token_urlsafe(7)
         url_col.insert_one({short:long})
     else:
         short= 'http://smol/'+alias
-        url_col.insert_one({short:long})
+        if url_col.find_one({short: {'$exists':True}}) != None:
+            print("invalid name already taken")
+            if allowMod==True:
+                short = "http://smol/" +alias+"/" +secrets.token_urlsafe(7)
+                url_col.insert_one({short:long})
+            else:
+                return 'error'
+        else:
+            url_col.insert_one({short:long})
     print(short)
-    return
+    return short
