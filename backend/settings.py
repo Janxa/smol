86249e90acdd4fe9c876c528b19dfd716c4a7ca3
Janxa <onizuka-54@live.fr>
2022-09-14 09:21:50 +0000

    Refactoring the backend to create a generate endpoint, correctly updating the state in urlFrom.jsx using component did update hook

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,4 @@
 import os
 mongo_url ="mongodb://127.0.0.1:27017"
 db_name='urls'
+domain_name="http://smol/"
--- backend/shortner/shortner.py
+++ backend/shortner/shortner.py
@@ -1,26 +1,24 @@
 from genericpath import exists
-from flask import Flask, request, Blueprint
+from flask import Flask, request, Blueprint,make_response
 import secrets
 from backend.extensions import url_col
+from backend.settings import domain_name
 
 shortner = Blueprint('shortner',__name__, url_prefix='/shortner')
-@shortner.route('',methods=['POST','GET'])
-def get_urls():
+@shortner.route('/generate',methods=['POST'])
+
+def generate_url():
     x = request.get_json()
     long,alias,allowMod = x['url'],x['alias'],x['allowMod']
-    response  = generate_url( long,alias,allowMod)
-    return response
-
-def generate_url(long,alias='',allowMod=False):
     if alias == '':
-        short = "http://smol/" + secrets.token_urlsafe(7)
+        short = domain_name + secrets.token_urlsafe(7)
         url_col.insert_one({short:long})
     else:
-        short= 'http://smol/'+alias
+        short= domain_name+alias
         if url_col.find_one({short: {'$exists':True}}) != None:
             print("invalid name already taken")
             if allowMod==True:
-                short = "http://smol/" +alias+"/" +secrets.token_urlsafe(7)
+                short = domain_name +alias+"/" +secrets.token_urlsafe(7)
                 url_col.insert_one({short:long})
             else:
                 return 'error'
@@ -28,4 +26,14 @@ def generate_url(long,alias='',allowMod=False):
             
             url_col.insert_one({short:long})
     print("inserted" ,long, " as = >",short, " and its _id is",url_col.find_one({short: {'$exists':True}})['_id'])
-    return [{'long':long,'short':short}]
+    response = make_response({long:short},200)
+    print('response',response)
+    return response
+
+@shortner.route('/retrieve',methods=['GET'])
+
+def retrieve_url():
+    x = request.get_json()
+    long,alias = x['url'],x['alias']
+    response  = url_col.find_one()
+    return response
