5b4d8557d7a7960d8fdf04050595b04e88a6aac8
Janxa <onizuka-54@live.fr>
2022-09-26 06:55:07 +0000

    Creating some basic error handling

--- client/src/components/UrlForm.jsx
+++ client/src/components/UrlForm.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import url_regex from "../variables";
+import axios from "axios";
 const Joi = require("joi");
 class UrlForm extends Component {
-  state = { data: { long: "", alias: "" } };
+  state = { data: { long: "", alias: "", allowMod: false }, errors: {} };
   schema = Joi.object({
     long: Joi.string().pattern(url_regex).required(),
-    alias: Joi.string().required(),
+    alias: Joi.string().min(3).required(),
   });
   handleChange = ({ currentTarget: input }) => {
     const name = input.name;
@@ -13,32 +14,53 @@ class UrlForm extends Component {
     data[name] = input.value;
     this.setState({ data });
   };
-
+  handleCheck = ({ currentTarget: checkBox }) => {
+    const name = checkBox.name;
+    const data = { ...this.state.data };
+    data[name] = !data[name];
+    this.setState({ data });
+  };
   handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(
-      this.schema.validate({
-        long: this.state.data["long"],
-        alias: this.state.data["alias"],
-      })
-    );
+    const data = { ...this.state.data };
+    let errors = { ...this.state.errors };
+    try {
+      await this.schema.validateAsync({
+        long: data["long"],
+        alias: data["alias"],
+      });
+      await axios.post("shortner/generate", data);
+    } catch (err) {
+      errors = err;
+      this.setState({ errors }, () => {
+        console.log("error", this.state.errors);
+      });
+    }
   };
   render() {
+    const data = { ...this.state.data };
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="long">Enter the url you want to shorten</label>
         <input
-          value={this.state.data.long}
+          value={data.long}
           onChange={this.handleChange}
           name="long"
           type="text"
         />
         <label htmlFor="alias">Customize your url</label>
         <input
-          value={this.state.data.alias}
+          value={data.alias}
           onChange={this.handleChange}
           name="alias"
           type="text"
+        />
+
+        <input
+          type="checkbox"
+          name="allowMod"
+          checked={data.allowMod}
+          onChange={this.handleCheck}
         />
         <button type="submit">Make it SMOL !</button>
       </form>
