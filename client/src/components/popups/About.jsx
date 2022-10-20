40ffd816defeeb339b681ed7ebf3e6fb80e38c43
Janxa <onizuka-54@live.fr>
2022-10-20 16:03:55 +0000

    Quick variable fix, domain name is now correctly set to http://smol.com/ instead of http://smol/

--- client/src/components/popups/About.jsx
+++ client/src/components/popups/About.jsx
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 function About() {
-  return <div>About smol</div>;
+  return (
+    <div>
+      <h3>About smol</h3>
+      <p>Smol is a small demo project I built to shorten urls.</p>
+      <p>
+        Its main goal is to display my coding skills, using react and python
+        flask in a quick and interesting project
+      </p>
+      <p>
+        As a demo, smol's database will not keep your urls more than 72 hours.
+      </p>
+      <p>
+        Smol is an open source project, you can check out how it's built in in
+        its github repository => http://smol.com/Janxa (see what i did there ?
+        :)).
+      </p>
+      <p>
+        If you have any question, feedback, or if you encountered a bug, you can
+        contact me using the contact form of the website, I check my mails
+        everyday !
+      </p>
+    </div>
+  );
 }
 
 export default About;
