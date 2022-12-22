b3cc667e1e02badeeac8397b8b98693ee11d39c6
Janxa <onizuka-54@live.fr>
2022-12-22 19:25:13 +0100

    Smol title font is now Roboto Bold

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = ({ToggleSidebar}) => {
   return (
     <header className="bg-stone-800 h-24 px-6 py-2 flex items-center justify-around shadow-sm shadow-stone-800 z-20 ">
       <div className="w-2/3">
-        <h1 className="text-secondary-yellow font-uncut font text-5xl">SMOL</h1>
-        <h2 className="text-secondary-yellow font-uncut text-xl" >Your urls, SMOLER</h2>
+        <h1 className="text-secondary-yellow font-bold text-5xl">SMOL</h1>
+        <h2 className="text-secondary-yellow text-xl" >Your urls, SMOLER</h2>
       </div>
       <div className="w-1/3 h-3/5 flex flex-col justify-around items-end ">
       <button onClick={ToggleSidebar} className="w-1/2 rounded-md px-2 h-full flex flex-col justify-around items-center hover:scale-105 transition-transform ease-in-out duration-100">
