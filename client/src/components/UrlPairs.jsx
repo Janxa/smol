2c391f1360c4de8a75598672c629eea12350cee0
Janxa <onizuka-54@live.fr>
2022-12-23 11:19:42 +0100

    Links are now troncated when too long, clicking link opens it in a new tab

--- client/src/components/UrlPairs.jsx
+++ client/src/components/UrlPairs.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 function UrlPairs(props) {
   return (
-    <div>
+    <div className="w-4/5 flex flex-col">
       <p onClick={null}>{props.data.short}</p>
-      <a href={props.data.long}>{props.data.long}</a>
+      <a className="truncate w-full" href={props.data.long} target="_blank" rel="noopener" > {props.data.long}</a>
     </div>
   );
 }
