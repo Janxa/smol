4e58bb4b9080d0b26ee770f47a3448d4f042307d
Janxa <onizuka-54@live.fr>
2022-12-26 09:37:18 +0100

    Styling url pairs

--- client/src/components/UrlPairs.jsx
+++ client/src/components/UrlPairs.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 function UrlPairs(props) {
   return (
-    <div className="w-4/5 flex flex-col">
-      <p onClick={null}>{props.data.short}</p>
-      <a className="truncate w-full" href={props.data.long} target="_blank" rel="noopener" > {props.data.long}</a>
+    <div className="w-4/5 pt-2 flex flex-col">
+      <p className="truncate w-full font-medium  text-primary-green"onClick={null}>{props.data.short}</p>
+      <a className="truncate w-full text-xs text-stone-700" href={props.data.long} target="_blank" rel="noreferrer" > {props.data.long}</a>
     </div>
   );
 }
