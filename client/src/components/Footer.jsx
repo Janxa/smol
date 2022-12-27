0803f3bcf77d5c24aaeeaa15c46c7b3f064b355c
Janxa <onizuka-54@live.fr>
2022-12-27 15:56:51 +0100

    Extracting props

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,26 +4,27 @@ import Contact from "./popups/Contact";
 import Help from "./popups/Help";
 import About from "./popups/About";
 function Footer(props) {
+  const {OpenPopup} = props;
   return (
     <footer className="bg-primary-brown fixed bottom-0  w-full" >
-      <ul className="flex justify-around items-center">
-        <li
-          onClick={() => props.OpenPopup(About )}
+      <ul className="flex justify-around items-center [&>*]:text-primary-white [&>*]:cursor-pointer ">
+        <li className=""
+          onClick={() => OpenPopup(About)}
         >
           About
         </li>
-        <li onClick={() => props.OpenPopup(Help )}>
+        <li onClick={() => OpenPopup(Help)}>
           Help
         </li><li>
-        <h3 className="text-xl font-medium text-secondary-yellow ">SMOL</h3>
+        <h3 className="text-xl font-medium text-secondary-yellow cursor-default ">SMOL</h3>
         </li>
         <li
-          onClick={() => props.OpenPopup( Privacy )}
+          onClick={() => OpenPopup( Privacy )}
         >
           Privacy
         </li>
         <li
-          onClick={() => props.OpenPopup(Contact )}
+          onClick={() => OpenPopup(Contact )}
         >
           Contact
         </li>
