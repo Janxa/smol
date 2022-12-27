d1c1ea7cfe0c3a32381243d76ee004b9aafd0af9
Janxa <onizuka-54@live.fr>
2022-12-27 19:07:16 +0100

    Various fixes to make the popup animation work smoothly

--- client/src/components/popups/Popup.jsx
+++ client/src/components/popups/Popup.jsx
@@ -6,28 +6,41 @@ function Popup(props) {
   useEffect(() => {
       function handleClickOutside(event) {
         console.log(PopupRef)
-
-        if (PopupRef.current && !PopupRef.current.contains(event.target)) {
-          ClosePopup()
+        if (PopupRef.current && !PopupRef.current.contains(event.target)  ){
+          animatedClosePopup()
         }
       }
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("click", handleClickOutside,{capture:true});
       return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
+        console.log("unmounting")
+        document.removeEventListener("click", handleClickOutside,{capture:true});
       };
     }, [PopupRef]);
-  
 
-  
-  
-
-  return( (visible) ? (
-  <div ref={PopupRef} >
-     <button onClick={props.ClosePopup}>X</button>
-     <Content ClosePopup={props.ClosePopup}/>
-  </div> )
+    useEffect(()=>{
+    if (visible){
+    setTimeout(()=>PopupRef.current.className="scale-100 rounded-lg p-4 top-1/2 left-1/2 h-5/6 w-5/6 fixed z-50  -translate-x-1/2 -translate-y-1/2 absolute bg-stone-200 transition-transform ease-in duration-300",10)
+    }},[visible],
+ 
+    )
     
-    : '');
+    const animatedClosePopup= () =>{ 
+      PopupRef.current.className="scale-0 top-1/2 left-1/2 h-5/6 w-5/6 fixed z-50 -translate-x-1/2   -translate-y-1/2 bg-stone-200 transition-transform ease-in-out duration-300"
+      setTimeout(ClosePopup,300);
+};
+    console.log("ref",PopupRef)
+
+  return( 
+  <div ref={PopupRef} className= "scale-0 top-1/2 left-1/2 h-5/6 w-5/6  z-50 absolute -translate-x-1/2 -translate-y-1/2  bg-stone-200 transition-transform ease-in duration-300">
+      <span onClick={animatedClosePopup}className="after:content-['\00d7'] after:cursor-pointer after:text-2xl after:text-primary-brown after:hover:text-black after:absolute after:hover:scale-105 after:top-0 after:right-3"></span>
+
+
+      <Content /> 
+     </div> 
+    
+  )
+    
+   
 }
 
 export default Popup;
