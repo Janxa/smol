73eaf14bfea7d995d27408d2268da33fc5e2fbcd
Janxa <onizuka-54@live.fr>
2022-12-28 19:57:47 +0100

    Redirection component, used to redirect and display a spinning wheel while loading

---
+++ client/src/components/Redirection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./spinner.css";
+import redirect from "../services/redirectService";
+import { useEffect } from "react";
+export default function Redirection() {
+  useEffect(()=>{
+    redirect()
+  })
+  return (<div className="grid justify-center items-center h-screen w-screen ">
+    <svg className="animate-spin h-30 w-30 mr-3 border-[15px] border-primary-brown border-t-secondary-yellow rounded-[50%]" viewBox="0 0 24 24">
+   </svg>
+   </div>
+  );
+}
