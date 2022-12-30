c912a475fc3d0c6bb28e4828c9f821f02c66bcf2
Janxa <onizuka-54@live.fr>
2022-12-30 18:38:47 +0100

    removing help component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
 import Privacy from "./popups/Privacy";
 import Contact from "./popups/Contact";
-import Help from "./popups/Help";
 import About from "./popups/About";
 function Footer(props) {
   const {OpenPopup} = props;
   return (
     <footer className="bg-primary-brown absolute bottom-0 z-20  w-full" >
-      <ul className="flex justify-around items-center [&>*]:text-primary-white [&>*:hover]:scale-105 [&>*:hover]:text-secondary-yellow [&>*]:cursor-pointer ">
+      <ul className="flex justify-around items-center   [&>*]:text-primary-white [&>*:hover]:scale-105 [&>*:hover]:text-secondary-yellow [&>*]:cursor-pointer ">
         <li
           onClick={() => OpenPopup(About)}
         >
           About
-        </li>
-        <li onClick={() => OpenPopup(Help)}>
-          Help
-        </li><li>
-        <h3 className="text-xl font-medium text-secondary-yellow cursor-default ">SMOL</h3>
-        </li>
-        <li
-          onClick={() => OpenPopup( Privacy )}
-        >
-          Privacy
         </li>
         <li
           onClick={() => OpenPopup(Contact )}
         >
           Contact
         </li>
+      <li>
+        <h3 className="text-xl font-medium text-secondary-yellow cursor-default ">SMOL</h3>
+        </li>
+        <li className="text-end"
+          onClick={() => OpenPopup( Privacy )}
+        >
+          Privacy
+        </li>
+        <li ></li>
       </ul>
     </footer>
   );
