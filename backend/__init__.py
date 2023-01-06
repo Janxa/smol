3f42af830cbba81a59d3fce7244206400c94a5f4
Janxa <onizuka-54@live.fr>
2023-01-06 17:19:58 +0100

    Changing static folder

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,10 +7,11 @@ from flask_mail import Mail
 
 
 def create_app() :
-    app = Flask(__name__)
+    app = Flask(__name__,static_folder="../client/build", static_url_path='/')
     app.config.from_object('backend.settings.app_config')
     mail.init_app(app)
     app.register_blueprint(contact)
     app.register_blueprint(shortner)
     app.register_blueprint(redirection)
     return app
+
