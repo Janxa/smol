ae7edd3b04b70c38391f0515b3feae9f5a81aa48
Janxa <onizuka-54@live.fr>
2023-01-19 16:00:20 +0100

    new build

--- client/src/services/redirectService.js
+++ client/src/services/redirectService.js
@@ -3,12 +3,11 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API_URL= process.env.REACT_APP_API_URL;
 
 async function redirect(){
-
     let url = window.location.href;
     let start = url.indexOf(BASE_URL);
     let end = start + BASE_URL.length;
     const short_url= url.substring(0, start - 1) + url.substring(end);
-
+    console.log('redirectcalled', 'url:',url,'start',start,'end',end,'short_url',short_url,'BASE_URL',BASE_URL,'API_URL',API_URL)
     if (url != BASE_URL){
         try{
         const res= await axios.get(API_URL+'/api/redirect/'+short_url);
