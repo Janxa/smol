dde9b9807871a4cbb2755e36ea03ed89599f863d
Janxa <onizuka-54@live.fr>
2023-01-19 18:41:53 +0100

    Quick fix

--- client/src/services/redirectService.js
+++ client/src/services/redirectService.js
@@ -1,13 +1,20 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-const API_URL= process.env.REACT_APP_API_URL;
 
 async function redirect(){
+    console.log(Object.keys(process.env))
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+    const API_URL= process.env.REACT_APP_API_URL;
+    console.log('redirectcalled')
+    console.log('BASE_URL',BASE_URL)
+    console.log('API_URL',API_URL)
     let url = window.location.href;
+    console.log('url:',url)
     let start = url.indexOf(BASE_URL);
+    console.log('start',start)
     let end = start + BASE_URL.length;
+    console.log('end',end)
     const short_url= url.substring(0, start - 1) + url.substring(end);
-    console.log('redirectcalled', 'url:',url,'start',start,'end',end,'short_url',short_url,'BASE_URL',BASE_URL,'API_URL',API_URL)
+    console.log('short_url',short_url)
     if (url != BASE_URL){
         try{
         const res= await axios.get(API_URL+'/api/redirect/'+short_url);
