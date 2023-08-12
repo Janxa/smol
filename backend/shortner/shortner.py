009700d646452094394e586d17797a23ff2674fa
Janxa <onizuka-54@live.fr>
2023-08-12 17:31:24 +0200

    Renaming name to alias in error message

--- backend/shortner/shortner.py
+++ backend/shortner/shortner.py
@@ -14,7 +14,7 @@ def generate():
     try:
       generated_url=generate_url(long_url,alias,allowMod,time)
     except NameError:
-        return make_response({"error":"Name already taken"},409)
+        return make_response({"error":"Alias already taken"},409)
     except Exception:
             return ({"error":"Server error, try again later"},500)
     return make_response({'urls':generated_url}, 200)
