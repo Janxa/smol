9c7c46451dde12d4f0a249dc2cd44d0c03b89ac3
Janxa <onizuka-54@live.fr>
2023-08-12 15:32:03 +0200

    Added error handling

--- backend/shortner/shortner.py
+++ backend/shortner/shortner.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request,current_app
+from flask import Blueprint, request,make_response
 from backend.services import generate_url ,delete_url
 from backend.extensions import url_collection
 
@@ -6,21 +6,20 @@ from datetime import datetime
 
 
 shortner = Blueprint('shortner',__name__, url_prefix='/api/shortner')
-@shortner.route('/generate',methods=['POST'])
 
+@shortner.route('/generate',methods=['POST'])
 def generate():
     x = request.get_json()
     long_url,alias,allowMod,time = x['url'],x['alias'],x['allowMod'],datetime.utcnow()
-    generated_url=generate_url(long_url,alias,allowMod,time)
-    return generated_url, 200
+    try:
+      generated_url=generate_url(long_url,alias,allowMod,time)
+    except NameError:
+        return make_response({"error":"Name already taken"},409)
+    except Exception:
+            return ({"error":"Server error, try again later"},500)
+    return make_response({'urls':generated_url}, 200)
 
-# @shortner.route('/retrieve',methods=['GET'])
 
-# def retrieve_url():
-#     x = request.get_json()
-#     long,alias = x['url'],x['alias']
-#     response  = url_collection.find_one()
-#     return response
 
 @shortner.route('/delete',methods=['DELETE'])
 def delete():
