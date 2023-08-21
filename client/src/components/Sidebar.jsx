50e4dfa4d5b2ea56d3a68f30afdeb72377e4650a
Janxa <onizuka-54@live.fr>
2023-08-21 16:38:58 +0200

    Improving display on large screen

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,53 +1,74 @@
 import React from "react";
-import axios  from "axios";
+import axios from "axios";
 import UrlPairs from "./UrlPairs";
+import Button from "./Common/Button";
 import { withCookies } from "react-cookie";
 function Sidebar(props) {
-  const {url_list,setUrl_list,visible} = props;
+	const { url_list, setUrl_list, visible, setVisible } = props;
 
+	const delete_url_from_list = async (id) => {
+		const new_url_list = [...url_list];
+		let deleted = new_url_list.splice(id, 1)[0];
 
-  const delete_url_from_list = async (id) => {
-    const new_url_list = [...url_list];
-    let deleted = new_url_list.splice(id, 1)[0];
+		const res = await axios.delete("/api/shortner/delete", { data: deleted });
+		setUrl_list(new_url_list);
+	};
 
-    // trycatch here to check server before updating cookie
+	return (
+		<aside
+			className={
+				(!visible
+					? "opacity-0 lg:opacity-100 translate-x-full "
+					: "opacity-100  translate-x-0  ") +
+				" fixed z-10 right-0 h-screen w-full lg:w-1/3 bg-stone-100 flex flex-col transition-[transform,opacity] ease-in duration-300  "
+			}
+		>
+			<div
+				onClick={() => setVisible(!visible)}
+				className={
+					"hidden lg:block bg-stone-100 hover:bg-secondary-yellow w-24 h-32  z-0 cursor-pointer shadow-sm  relative top-1/2 left-0 -translate-x-1/2 rounded-full transition-colors duration-300 "
+				}
+			>
+				<span
+					className={
+						(!visible ? "-rotate-[135deg]" : "rotate-45 -translate-x-1/2") +
+						"  border-stone-800 p-3 absolute -translate-y-1/2 top-1/2 left-1/4 inline-block border-t-8 border-r-8 transition-all  duration-300"
+					}
+				></span>
+			</div>
+			<div className="px-6 py-4 w-full h-full z-10 bg-stone-100 overflow-scroll">
+				<h3 className="text-2xl lg:text-4xl xl:text-5xl  font-medium text-stone-900">
+					Your recent smol urls:
+				</h3>
+				<ul className="flex flex-col h-full">
+					{url_list.length > 0 ? (
+						url_list.map((url_pair, id) => (
+							<li key={id} className=" py-2 border-b-2 border-primary-brown-2">
+								<UrlPairs data={url_pair} />
+								<div className="pb-2 pt-3 xl:pt-4 flex justify-around w-2/3 lg:w-full 2xl:w-2/3">
+									<Button
+										color="green"
+										onClick={null}
+										text="Copy to clipboard"
+									/>
 
-    const res = await axios.delete("/api/shortner/delete", { data: deleted });
-    console.log(res)
-    setUrl_list(new_url_list)
-
-
-  };
-
-
-  return (
-    <aside  className={visible
-      ? "opacity-100 fixed z-10 top-24 2xl:top-28 right-0 h-screen w-full translate-x-0 bg-stone-200 flex flex-col transition-[transform,opacity]  ease-in duration-300  p-4"
-      : "opacity-0 z-10 fixed  top-24 2xl:top-28  right-0  h-screen w-full  translate-x-full bg-stone-200 flex flex-col transition-[transform,opacity] ease-in duration-300 p-4"
-      }>
-
-      <h3 className="text-2xl  font-medium">Your recent smol urls</h3>
-      <ul className="flex flex-col h-full">
-        {/* If there's a url in the list, render list, else render no urls */}
-        {url_list.length >0
-          ?(url_list.map((url_pair, id) => (
-            <li key={id} className=" py-2 border-b-2 border-primary-brown-2">
-              <UrlPairs data={url_pair} />
-              <div className="pb-2 pt-3 flex justify-around w-2/3">
-              <button className="btn-validation" onClick={null}>
-                Copy Smol url
-              </button>
-              <button className="btn-danger" onClick={() => delete_url_from_list(id)}>
-                Delete url
-              </button>
-              </div>
-            </li>
-             )))
-         :<li className=" text-l pt-8 place-self-center">No urls saved yet</li>}
-
-      </ul>
-    </aside>
-  );
+									<Button
+										color="red"
+										onClick={() => delete_url_from_list(id)}
+										text="Delete url"
+									/>
+								</div>
+							</li>
+						))
+					) : (
+						<li className=" text-lg pt-8 place-self-center">
+							No urls saved yet
+						</li>
+					)}
+				</ul>
+			</div>
+		</aside>
+	);
 }
 
 export default withCookies(Sidebar);
