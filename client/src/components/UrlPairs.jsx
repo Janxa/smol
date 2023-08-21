4713eb9d7e5302db535b9e3e48db63fe08421dcd
Janxa <onizuka-54@live.fr>
2023-08-21 16:58:28 +0200

    Reworking font size for smaller screens

--- client/src/components/UrlPairs.jsx
+++ client/src/components/UrlPairs.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 function UrlPairs(props) {
-  return (
-    <div className="w-4/5 pt-2 flex flex-col">
-      <p className="truncate w-full font-medium  text-primary-green"onClick={null}>{props.data.short}</p>
-      <a className="truncate w-full text-xs text-stone-700" href={props.data.long} target="_blank" rel="noreferrer" > {props.data.long}</a>
-    </div>
-  );
+	return (
+		<div className="w-4/5 pt-2 pl-4 flex flex-col">
+			<p
+				className="truncate w-full font-medium text-primary-green lg:text-xl md:text-lg"
+				onClick={null}
+			>
+				{props.data.short}
+			</p>
+			<a
+				className="truncate w-full text-xs md:text-sm text-stone-700 lg:text-lg"
+				href={props.data.long}
+				target="_blank"
+				rel="noreferrer"
+			>
+				{" "}
+				{props.data.long}
+			</a>
+		</div>
+	);
 }
 
 export default UrlPairs;
