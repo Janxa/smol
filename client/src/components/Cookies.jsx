3b3e971405d5064405a3bccfb5af6f4e685f2756
Janxa <onizuka-54@live.fr>
2023-08-21 19:21:32 +0200

    Cookie bar is now using Button component

--- client/src/components/Cookies.jsx
+++ client/src/components/Cookies.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-
+import Button from "./Common/Button";
 function CookiesBanner(props) {
-  return (
-    <div className="z-20 shadow-2xl fixed bottom-0 left-0 right-0 w-full bg-stone-200 p-4 flex flex-col items-center ">
-      <div className="h-full  flex flex-col justify-around items-center">
-      <p className="text-stone-900 font p-4 text-lg ">We use cookies to personalize and improve your experience on this site. Visit our privacy policy for more information on our data collection practices. </p>
-      <div className="flex w-2/4 justify-around"   >
-        <button  className="btn-validation mx-2"onClick={() => props.CreateCookie(props.url_list)}>Accept</button>
-        <button className="btn-danger   mx-2" onClick={()=>props.RefuseCookie()} >Refuse</button>
-      </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="z-20 shadow-2xl fixed bottom-0 left-0 right-0 w-full bg-stone-200 p-4 flex flex-col items-center ">
+			<div className="h-full  flex flex-col justify-around items-center">
+				<p className="text-stone-900 font p-4 text-lg ">
+					We use cookies to personalize and improve your experience on this
+					site. Visit our privacy policy for more information on our data
+					collection practices.{" "}
+				</p>
+				<div className="flex w-2/4 justify-around">
+					<Button
+						onClick={() => props.CreateCookie(props.url_list)}
+						color="green"
+						text="Accept"
+					/>
+					<Button
+						onClick={() => props.CreateCookie(props.url_list)}
+						color="red"
+						text="Refuse"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CookiesBanner;
