b125e9940b48f179a0e9201989c63abf1fffc854
Janxa <onizuka-54@live.fr>
2023-08-22 19:37:29 +0200

    Removing unused imports

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask, current_app
+from flask import Flask
 from backend.contact.contact import contact
 from backend.shortner.shortner import shortner
 from backend.redirect.redirect import redirection
 from backend.extensions import mail
-from flask_mail import Mail
 
 
 def create_app() :
+
     app = Flask(__name__,static_folder="../build", static_url_path='/')
     app.config.from_object('backend.settings.app_config')
     mail.init_app(app)
@@ -19,4 +19,5 @@ def create_app() :
     @app.route('/<path>')
     def renderindex(path):
         return app.send_static_file('index.html')
+
     return app
