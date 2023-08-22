f72e589ad43607edcafb603ee658fe957e54d599
Janxa <onizuka-54@live.fr>
2023-08-22 19:52:39 +0200

    Writing privacy policy

--- client/src/components/popups/Privacy.jsx
+++ client/src/components/popups/Privacy.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 function Privacy() {
-  return <div>Privacy</div>;
+	return (
+		<div>
+			<h2 className="font-bold text-xl">Smol URL Shortener - Privacy Policy</h2>
+			<p className="text-xs">Effective Date: 22/04/2023</p>
+			<p>
+				This page outlines smol's approach to privacy and how it handles your
+				data.
+			</p>
+			<h3 className="font-bold pt-2">1. No Data Collection</h3>
+			Smol keep things simple and respect your privacy. It do not collect any
+			personal information or data from its users. It only collect the long URL
+			you submit for shortening and the corresponding shortened URL.
+			<h3 className="font-bold pt-2"> 2. Cookies for Convenience</h3>
+			Smol uses cookies to make your experience smoother. The cookies it uses
+			are there only to keep track of your session and help you retrieve the
+			original URL associated with the shortened link.
+			<h3 className="font-bold pt-2"> 3. Your Data, Your Control</h3>
+			Your data is yours alone. Smol do not share, sell, or disclose any of the
+			information you provide. The short URLs you create are private and can
+			only be accessed by you or anyone you share them with.
+			<h3 className="font-bold pt-2"> 4. Changes and Updates</h3>
+			If smol's privacy practice changes, its policy will be updates and provide
+			a new effective date. So please check back occasionally to stay informed.
+			<h3 className="font-bold pt-2"> 5. Contact Me</h3>
+			If you have any questions or concerns about the privacy practices, feel
+			free to contact me via the contact form of this website.
+		</div>
+	);
 }
 
 export default Privacy;
