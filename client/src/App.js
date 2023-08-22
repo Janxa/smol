c84b93920f610d785f66d87ca2d3f33cfe7c89f9
Janxa <onizuka-54@live.fr>
2023-08-22 18:09:22 +0200

    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import {  ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import React, { useEffect, useState } from "react";
 import Main from "./components/Main.jsx";
 import Header from "./components/Header";
@@ -9,92 +9,91 @@ import Sidebar from "./components/Sidebar";
 import Popup from "./components/popups/Popup";
 import CookiesBanner from "./components/Cookies";
 import { useCookies } from "react-cookie";
+
 function App() {
+	const [cookies, setCookie] = useCookies([]);
+	const [cookieTabVisible, setCookieTabVisible] = useState(
+		cookies.url_list ? false : true
+	);
+	const [sidebarVisible, setSidebarVisible] = useState(false);
+	const [popup, setPopup] = useState({ content: null, visible: false });
+	const [url_list, setUrl_list] = useState(cookies.url_list || []);
 
+	useEffect(() => {
+		if (Object.keys(cookies).length > 0) {
+			setCookie("url_list", url_list, {
+				path: "/",
+			});
+		}
+	}, [url_list, setCookie, cookies]);
 
-  const [cookies, setCookie] = useCookies([]);
-  const [cookieVisible,setCookieVisible]= useState(cookies.url_list?false:true)
-  const [sidebarVisible,setSidebarVisible]=useState(false)
-  const [popup, setPopup] = useState({  content: null,visible: false});
-  const [ url_list, setUrl_list]= useState(((cookies.url_list) || [] ));
+	function CreateCookie(url_list) {
+		setCookie("url_list", url_list, {
+			path: "/",
+		});
 
-  useEffect(()=>{
-    if (Object.keys(cookies).length > 0) {
-      setCookie("url_list", url_list, {
-        path: "/",
-      });
-    }
-  },[url_list,setCookie,cookies]
-    )
-  function CreateCookie(url_list) {
-    setCookie("url_list", url_list, {
-      path: "/",
-    });
+		setCookieTabVisible(false);
+	}
+	function RefuseCookie() {
+		setCookieTabVisible(false);
+	}
 
-    setCookieVisible(false);
-  }
-  function RefuseCookie(){
-    setCookieVisible(false);
-  }
+	function OpenPopup(content) {
+		// setPopup({content:content,visible:true})
+		if (popup.content === content) {
+			return;
+		}
+		if (popup.visible) {
+			setTimeout(() => setPopup({ content: content, visible: true }), 400);
+		} else {
+			setPopup({ content: content, visible: true });
+		}
+	}
 
-  function OpenPopup(content) {
-    // setPopup({content:content,visible:true})
-    if (popup.content===content){
-      return
-    }
-     if (popup.visible){
-       setTimeout(()=>setPopup({content:content,visible:true}),400);}
-     else {setPopup({content:content,visible:true})};
-  }
+	function ClosePopup() {
+		setPopup({ content: null, visible: false });
+	}
+	function ToggleSidebar() {
+		if (sidebarVisible) {
+			setSidebarVisible(false);
+		} else {
+			setSidebarVisible(true);
+		}
+	}
+	return (
+		<div className="flex flex-col h-screen bg-stone-600 ">
+			<ToastContainer />
+			<Header ToggleSidebar={ToggleSidebar} sidebarVisible={sidebarVisible} />
 
-  function ClosePopup(){
-   setPopup({content:null,visible:false});
-  }
-  function ToggleSidebar(){
-    if (sidebarVisible) {
-      setSidebarVisible(false)
-    }
-    else{
-      setSidebarVisible(true)
-    }
-  }
-  return (
-    <div className="flex flex-col h-screen bg-stone-600 ">
-      <ToastContainer />
-      <Header ToggleSidebar={ToggleSidebar} sidebarVisible={sidebarVisible}/>
+			<Sidebar
+				url_list={url_list}
+				setUrl_list={setUrl_list}
+				visible={sidebarVisible}
+				setVisible={setSidebarVisible}
+				CreateCookie={CreateCookie}
+			/>
 
-      <Sidebar
-        url_list={url_list}
-        setUrl_list={setUrl_list}
-        visible={sidebarVisible}
-        cookieVisible={cookieVisible}
-      />
+			<Main url_list={url_list} setUrl_list={setUrl_list} />
 
-      <Main
-      url_list={url_list}
-      setUrl_list={setUrl_list}
+			<Footer OpenPopup={OpenPopup} />
 
-      />
+			{popup.visible && (
+				<Popup
+					Content={popup.content}
+					visible={popup.visible}
+					ClosePopup={ClosePopup}
+				/>
+			)}
 
-      <Footer OpenPopup={OpenPopup} />
-
-      {popup.visible &&
-      <Popup
-      Content={popup.content}
-      visible={popup.visible}
-      ClosePopup={ClosePopup}
-      />
-}
-
-      { cookieVisible && (
-          <CookiesBanner
-            RefuseCookie={RefuseCookie}
-            CreateCookie={CreateCookie}
-            url_list={url_list}
-          />
-        )}
-    </div>
-  );
+			{cookieTabVisible && (
+				<CookiesBanner
+					RefuseCookie={RefuseCookie}
+					CreateCookie={CreateCookie}
+					url_list={url_list}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default App;
