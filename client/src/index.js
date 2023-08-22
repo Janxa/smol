4cb340c57bdde2b1a58afa084ea82b0f7f2e3da3
Janxa <onizuka-54@live.fr>
2023-08-22 19:44:26 +0200

    Fixing indent

--- client/src/index.js
+++ client/src/index.js
@@ -4,23 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
-import {
-  Route,Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Redirection from "./components/Redirection";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <CookiesProvider>
-    <BrowserRouter>
-    <Routes>
-      <Route element={<App />} path="/" />
-      <Route element={<Redirection />} path="/:short"/>
-      </Routes>
-    </BrowserRouter>
-  </CookiesProvider>
+	<CookiesProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<App />} path="/" />
+				<Route element={<Redirection />} path="/:short" />
+			</Routes>
+		</BrowserRouter>
+	</CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
