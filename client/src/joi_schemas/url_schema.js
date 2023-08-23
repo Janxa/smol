aaf269c0bbb3bda34dda50622e58db7a3b47f81a
Janxa <onizuka-54@live.fr>
2023-08-23 16:14:13 +0200

    Switching error name for regex errors: string.regex > string.pattern.base

--- client/src/joi_schemas/url_schema.js
+++ client/src/joi_schemas/url_schema.js
@@ -3,25 +3,25 @@ import { url_regex, reserved_keywords } from "../variables";
 const Joi = require("joi");
 
 const url_schema = Joi.object({
-    url: Joi.string().pattern(url_regex).max(1000).required().messages({
-      "string.pattern.base":
-        "This is not a valid url, url should look like 'http://thisIsAnURL.com'",
-      "string.max": "Url too long",
-      "string.empty": "Please insert an url",
-    }),
-    alias: Joi.string()
-      .min(3)
-      .trim()
-      .max(1000)
-      .regex(/^\S+$/)
-      .allow("")
-      .invalid(...reserved_keywords)
-      .messages({
-        "string.min": "Alias too short",
-        "string.max": "Alias too long",
-        "string.invalid": "Alias invalid",
-        "string.regex": "Alias can't contain space",
-      }),
-  });
+	url: Joi.string().pattern(url_regex).max(1000).required().messages({
+		"string.pattern.base":
+			"This is not a valid url, url should look like 'http://thisIsAnURL.com'",
+		"string.max": "Url too long",
+		"string.empty": "Please insert an url",
+	}),
+	alias: Joi.string()
+		.min(3)
+		.trim()
+		.max(1000)
+		.regex(/^\S+$/)
+		.allow("")
+		.invalid(...reserved_keywords)
+		.messages({
+			"string.min": "Alias too short",
+			"string.max": "Alias too long",
+			"string.invalid": "Alias invalid",
+			"string.pattern.base": "Alias can't contain space",
+		}),
+});
 
-  export{url_schema}
+export { url_schema };
