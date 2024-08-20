fd1e7091c3b52b603a95df6215c811b83fc910d5
Janxa <bauer.maxime@outlook.com>
2024-08-20 15:31:50 +0200

    Improving readability

--- client/src/components/Common/Button.jsx
+++ client/src/components/Common/Button.jsx
@@ -1,20 +1,27 @@
-import React, { Component } from "react";
-const Button = ({ color, onClick, text, type, size }) => {
-	const style =
-		`
+import React from "react";
+
+const Button = ({ color, onClick, text, type, size: textSize = "md" }) => {
+	const sizeMapping = {
+		sm: "text-sm px-2 py-1",
+		md: "text-md lg:text-lg px-4 py-1",
+	};
+
+	const colorMapping = {
+		green: "bg-primary-green hover:bg-primary-green-2",
+		red: "bg-primary-red hover:bg-primary-red-2",
+	};
+
+	const style = `
     rounded-lg
     w-max
     text-primary-white
     shadow
     shadow-primary-brown
     active:shadow-primary-brown-2 active:shadow-inner
-    transition-all ease-in-out duration-200 ` +
-		(color == "green"
-			? ` bg-primary-green
-            hover:bg-primary-green-2
-      `
-			: ` bg-primary-red hover:bg-primary-red-2 `) +
-		(size == "small" ? " text-sm px-2 py-1" : ` text-md lg:text-lg  px-4 py-1`);
+    transition-all ease-in-out duration-200
+    ${colorMapping[color]}
+    ${sizeMapping[textSize]}
+  `;
 
 	return (
 		<button onClick={onClick} type={type} className={style}>
@@ -24,3 +31,4 @@ const Button = ({ color, onClick, text, type, size }) => {
 };
 
 export default Button;
+
