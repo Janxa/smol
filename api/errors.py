694ec3802defe7017937c729aaa386bec4f18ef9
Janxa <bauer.maxime@outlook.com>
2024-09-18 15:58:30 +0200

    Adding alias and mail related errors

--- api/errors.py
+++ api/errors.py
@@ -4,3 +4,15 @@ class DBError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+class AliasAlreadyExistsError(Exception):
+    """Raised when a custom alias is already taken and modification is not allowed."""
+    def __init__(self, alias):
+        self.alias = alias
+        super().__init__(f"Alias '{alias}' is already taken.")
+
+class MailTooLongError(Exception):
+    """Raised when a mail exceed the maximum size"""
+    def __init__(self, size_limit):
+        self.size_limit = size_limit
+        super().__init__(f"Mail is too long, max {size_limit}.")
