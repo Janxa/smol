d6f8deec66c553ae4a0276a83e715946e5fc3fb5
Janxa <bauer.maxime@outlook.com>
2024-09-18 19:30:25 +0200

    Destructuring props

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,33 +2,24 @@ import React from "react";
 import Privacy from "./popups/Privacy";
 import Contact from "./popups/Contact";
 import About from "./popups/About";
-function Footer(props) {
-  const {OpenPopup} = props;
-  return (
-    <footer className="bg-primary-brown absolute bottom-0 z-20  w-full" >
-      <ul className="flex justify-around items-center   [&>*]:text-primary-white [&>*:hover]:scale-105 [&>*:hover]:text-secondary-yellow [&>*]:cursor-pointer ">
-        <li
-          onClick={() => OpenPopup(About)}
-        >
-          About
-        </li>
-        <li
-          onClick={() => OpenPopup(Contact )}
-        >
-          Contact
-        </li>
-      <li>
-        <h3 className="text-xl font-medium text-secondary-yellow cursor-default ">SMOL</h3>
-        </li>
-        <li className="text-end"
-          onClick={() => OpenPopup( Privacy )}
-        >
-          Privacy
-        </li>
-        <li ></li>
-      </ul>
-    </footer>
-  );
+function Footer({ OpenPopup }) {
+	return (
+		<footer className="bg-primary-brown absolute bottom-0 z-20  w-full">
+			<ul className="flex justify-around items-center   [&>*]:text-primary-white [&>*:hover]:scale-105 [&>*:hover]:text-secondary-yellow [&>*]:cursor-pointer ">
+				<li onClick={() => OpenPopup(About)}>About</li>
+				<li onClick={() => OpenPopup(Contact)}>Contact</li>
+				<li>
+					<h3 className="text-xl font-medium text-secondary-yellow cursor-default ">
+						SMOL
+					</h3>
+				</li>
+				<li className="text-end" onClick={() => OpenPopup(Privacy)}>
+					Privacy
+				</li>
+				<li></li>
+			</ul>
+		</footer>
+	);
 }
 
 export default Footer;
