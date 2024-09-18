33c16de997c03106de515b64588b79e667d202ed
Janxa <bauer.maxime@outlook.com>
2024-09-18 14:59:34 +0200

    Creating service and error handling

--- api/contact/contact.py
+++ api/contact/contact.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, make_response, request
+from flask import Blueprint, current_app, make_response, request
 from .services import send_contact_email
 
 
@@ -10,5 +10,18 @@ def sendmail():
     if not data:
         return make_response({"error": "Invalid or missing JSON in request"}, 400)
 
-    mail_content, mail_sender = data['mail_content'],data['mail_sender']
-    return send_contact_email(mail_content,mail_sender)
+    mail_content, mail_sender = data.get('mail_content'), data.get('mail_sender')
+
+    if not mail_content or not mail_sender:
+        return make_response({"error": "Missing mail_content or mail_sender"}, 400)
+
+    try:
+        send_contact_email(mail_content, mail_sender)
+        return make_response("Email Successfully Sent", 200)
+
+    except ValueError as error:
+        return make_response(str(error), 413)
+
+    except Exception as error:
+        current_app.logger.exception(f'Error in send_contact_email: {error}')
+        return make_response("Something went wrong", 500)
--- api/contact/services.py
+++ api/contact/services.py
@@ -4,26 +4,16 @@ from api.extensions import mail
 from api.settings import app_config
 
 
-def send_contact_email(content: str, sender: str) -> Response:
+def send_contact_email(content: str, sender: str) -> None:
     subject = f"New email from {app_config.DOMAIN_NAME}'s contact form!"
 
-    try:
-        if len(content) > 10000:
-            raise ValueError("Mail too long")
+    if len(content) > 10000:
+        raise ValueError("Mail too long")
 
-        message = Message(
-            subject,
-            sender=current_app.config["MAIL_DEFAULT_SENDER"],
-            recipients=[current_app.config["MAIL_DEFAULT_SENDER"]],
-        )
-        message.body = f"Message sent by {sender}\n\n{content}"
-        mail.send(message)
-
-        return make_response("Email Successfully Sent", 200)
-
-    except ValueError as error:
-        return make_response(str(error), 413)
-
-    except Exception as error:
-        current_app.logger.exception(f'Error in send_contact_email: {error}')
-        return make_response("Something went wrong", 500)
+    message = Message(
+        subject,
+        sender=current_app.config["MAIL_DEFAULT_SENDER"],
+        recipients=[current_app.config["MAIL_DEFAULT_SENDER"]],
+    )
+    message.body = f"Message sent by {sender}\n\n{content}"
+    mail.send(message)
