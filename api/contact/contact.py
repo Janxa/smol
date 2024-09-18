6daff9f9aeff14026829c1a40fde6804f6e204be
Janxa <bauer.maxime@outlook.com>
2024-09-18 15:55:34 +0200

    Adding custom error and validate request data

--- api/contact/contact.py
+++ api/contact/contact.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, current_app, make_response, request
+from flask import Blueprint, current_app, make_response
+
+from api.errors import MailTooLongError
+from api.utils import validate_request_data
 from .services import send_contact_email
 
 
@@ -6,22 +9,19 @@ contact = Blueprint('contact',__name__, url_prefix='/api/contact')
 
 @contact.route('/send',methods=['POST'])
 def sendmail():
-    data = request.get_json()
-    if not data:
-        return make_response({"error": "Invalid or missing JSON in request"}, 400)
-
-    mail_content, mail_sender = data.get('mail_content'), data.get('mail_sender')
-
-    if not mail_content or not mail_sender:
-        return make_response({"error": "Missing mail_content or mail_sender"}, 400)
 
     try:
+        request_data = validate_request_data(["mail_content", "mail_sender"])
+        mail_content, mail_sender = request_data.get('mail_content'), request_data.get('mail_sender')
         send_contact_email(mail_content, mail_sender)
         return make_response("Email Successfully Sent", 200)
 
-    except ValueError as error:
-        return make_response(str(error), 413)
+    except KeyError as error:
+        return make_response({"error": {error} }, 400)
+
+    except MailTooLongError as error:
+        return make_response({"error": error }, 413)
 
     except Exception as error:
         current_app.logger.exception(f'Error in send_contact_email: {error}')
-        return make_response("Something went wrong", 500)
+        return make_response({"error": "Something went wrong"}, 500)
